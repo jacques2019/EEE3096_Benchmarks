dc2e249e1b9049838c1bdf42bf9f4f6690edd3ea
jacques2019 <jacques@DESKTOP-C33UBO6.localdomain>
2020-09-26 16:35:36 +0200

    Update script for flags

--- RunFlags.sh
+++ RunFlags.sh
@@ -7,10 +7,10 @@ echo "starting"
 for ((i=0;i<7;i++)) do
     echo ${param[$i]}
 
-    $(make -C C ${param[$i]})
-    $(./RunCBase.sh)
-    $(cp data/CBaseTemps.txt data/CBaseTemps_${param[$i]})
-    $(cp data/CBaseTemps.txt data/CBaseTemps_${param[$i]})
+    make -C C ${param[$i]})
+    ./RunCBase.sh)
+    cp data/CBaseTemps.txt data/CBaseTemps_${param[$i]}
+    cp data/CBaseTemps.txt data/CBaseTemps_${param[$i]}
 done
 
 echo "done"
