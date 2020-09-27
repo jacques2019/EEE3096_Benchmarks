f96da639005f08f2fe0a896fbdbada339de825b6
Tim <sparky.tim.young@gmail.com>
2020-09-27 17:28:19 +0100

    16 bit and 64 bit for threaded C

---
+++ RunC16BitThreaded.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -a times
+declare -a temps
+
+echo "Running C 16 bit Threaded Benchmark"
+
+for ((i=0;i<200;i++))do
+	time=( $(make -C C run_threaded) )
+	times[$i]=${time[23]}
+	temp=$(vcgencmd measure_temp)
+	temps[$i]=${temp:5:4}
+
+	echo $i ${temps[$i]}
+
+done
+
+printf "%s\n" "{times[@]}" > data/C16BitThreadedTimes.txt
+printf "%s\n" "{temps[@]}" > data/C16BitThreadedTemps.txt
+
+echo "done"
